483d212295e9dd418e37aec3024ae0ecadbd1a48
hai300420 <haidoi3004@gmail.com>
2025-03-08 08:07:35 +0700

    Fix hit box collider 2 times, Speed UI, Speed based on angle

--- PRU212_SnowBoarder/Assets/Scripts/GameManager.cs
+++ PRU212_SnowBoarder/Assets/Scripts/GameManager.cs
@@ -6,19 +6,26 @@ public class GameManager : MonoBehaviour
     public static int score;
     public static int highestScore = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI speedText; // Add a UI text for speed
+
+    private PlayerController playerController; // Reference to PlayerController
 
     public static int Flag = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerController = FindAnyObjectByType<PlayerController>(); // Find the PlayerController in the scene
         UpdateScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (playerController != null)
+        {
+            UpdateSpeed(); // Update speed UI every frame
+        }
     }
     public void AddScore(int points)
     {
@@ -29,5 +36,9 @@ public class GameManager : MonoBehaviour
     {
         scoreText.text = "Score: " + score.ToString();
         highestScore = Mathf.Max(score, highestScore);
+    }
+    void UpdateSpeed()
+    {
+        speedText.text = "Speed: " + playerController.GetSpeed().ToString("F1"); // Display speed with 1 decimal place
     }
 }
